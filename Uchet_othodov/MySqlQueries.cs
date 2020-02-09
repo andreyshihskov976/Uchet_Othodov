89e0617164dc6af470afd79a2d1e313f62486ba6
Андрей Шишков <andreyshishkov976@outlook.com>
2020-02-10 02:29:35 +0300

    10.02.2020

--- Uchet_othodov/MySqlQueries.cs
+++ Uchet_othodov/MySqlQueries.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Uchet_othodov
+﻿namespace Uchet_othodov
 {
     public class MySqlQueries
     {
